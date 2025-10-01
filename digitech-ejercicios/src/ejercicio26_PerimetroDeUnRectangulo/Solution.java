219e31e06b04bc055a6ad0bde2039c69d510fd5d
Juan Manuel Galán Pérez <juanmagape.trb@gmail.com>
2025-10-01 12:01:21 +0200

    Cambio ejercicio

--- digitech-ejercicios/src/ejercicio26_PerimetroDeUnRectangulo/Solution.java
+++ digitech-ejercicios/src/ejercicio26_PerimetroDeUnRectangulo/Solution.java
@@ -13,8 +13,8 @@ public class Solution {
         int x2 = print.nextInt();
         int y2 = print.nextInt();
 
-        int base = Math.abs(x1 - x2);
-        int altura = Math.abs(y1 - y2);
+        int base = (x1 - x2);
+        int altura = (y1 - y2);
 
         int perimetro = 2 * (base + altura);
 
