c88230ca1d82afaab2da2945dae3785dbb8c81dd
juanmagape <juanmagape.trb.@gmail.com>
2025-10-25 23:21:40 +0200

    Prueba

--- digitech-ejercicios/src/pruebas/Solution.java
+++ digitech-ejercicios/src/pruebas/Solution.java
@@ -6,9 +6,9 @@ public class Solution {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        String texto = sc.nextLine();
+        String palabra = sc.nextLine();
 
-        String palabras [] = texto.split(" ");
+        String palabras [] = palabra.split(" ");
 
         for (int i = 0; i <= palabras.length; i++) {
             System.out.println(palabras[i]);
